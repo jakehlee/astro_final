04fde69e853a12f91c02acb169ed981df2b3fc3f
Jake Lee <jakehlee1@gmail.com>
2019-07-01 14:23:17 -0700

    Bad spaxel filtering attempts using 2D products

--- export_marvin.py
+++ export_marvin.py
@@ -3,12 +3,44 @@ import numpy as np
 import marvin
 import csv
 
-IFU = '9883-3701'
+IFU = '9871-12705'
+
+ATTRS = [
+	"emline_gflux_oii_3727",
+	"emline_gflux_oii_3729",
+	"emline_gflux_hthe_3798",
+	"emline_gflux_heta_3836",
+	"emline_gflux_neiii_3869",
+	"emline_gflux_hzet_3890",
+	"emline_gflux_neiii_3968",
+	"emline_gflux_heps_3971",
+	"emline_gflux_hdel_4102",
+	"emline_gflux_hgam_4341",
+	"emline_gflux_heii_4687",
+	"emline_gflux_hb_4862",
+	"emline_gflux_oiii_4960",
+	"emline_gflux_oiii_5008",
+	"emline_gflux_hei_5877",
+	"emline_gflux_oi_6302",
+	"emline_gflux_oi_6365",
+	"emline_gflux_nii_6549",
+	"emline_gflux_ha_6564",
+	"emline_gflux_nii_6585",
+	"emline_gflux_sii_6718",
+	"emline_gflux_sii_6732"
+]
 
 # Get datacube from Manga
 print('Importing', IFU, 'from MaNGA Marvin...')
 cube = marvin.tools.Cube(plateifu = IFU)
-#cube = marvin.tools.Cube(plateifu = '8257-6103')
+maps = marvin.tools.Maps(IFU)
+ha = maps.emline_gflux_ha_6564
+
+for attr in ATTRS:
+	curr_ha = getattr(maps, attr)
+	ha = ha + curr_ha
+
+nocov = ha.masked.mask
 
 # Get spectra (flux)
 flux = cube.flux
@@ -40,6 +72,9 @@ for i in range(side_len):
 csv_list = []
 for i in range(side_len):
 	for j in range(side_len):
+		# if masked data, do not export
+		if nocov[i,j]:
+			continue
 		temp = exported[side_len*i+j].tolist()
 		label = str(j)+"-"+str(i)
 		temp.insert(0, label)
--- vis_results.py
+++ vis_results.py
@@ -8,9 +8,34 @@ from matplotlib.backends.backend_pdf import PdfPages
 import marvin
 import csv
 
-DEMUD_PATH = '/Users/Jake/Documents/GitHub/DEMUD/demud/results/cnn-k=50-dim=4563-full-init_item=svd-9883-3701/'
-IFU = '9883-3701'
+DEMUD_PATH = '/Users/Jake/Documents/GitHub/DEMUD/demud/results/cnn-k=50-dim=4563-full-init_item=svd-9871-12705/'
+IFU = '9871-12705'
 N = 10
+
+ATTRS = [
+	"emline_gflux_oii_3727",
+	"emline_gflux_oii_3729",
+	"emline_gflux_hthe_3798",
+	"emline_gflux_heta_3836",
+	"emline_gflux_neiii_3869",
+	"emline_gflux_hzet_3890",
+	"emline_gflux_neiii_3968",
+	"emline_gflux_heps_3971",
+	"emline_gflux_hdel_4102",
+	"emline_gflux_hgam_4341",
+	"emline_gflux_heii_4687",
+	"emline_gflux_hb_4862",
+	"emline_gflux_oiii_4960",
+	"emline_gflux_oiii_5008",
+	"emline_gflux_hei_5877",
+	"emline_gflux_oi_6302",
+	"emline_gflux_oi_6365",
+	"emline_gflux_nii_6549",
+	"emline_gflux_ha_6564",
+	"emline_gflux_nii_6585",
+	"emline_gflux_sii_6718",
+	"emline_gflux_sii_6732"
+]
 
 demud_files = os.listdir(DEMUD_PATH)
 recon = []
@@ -54,7 +79,12 @@ xunit = cube.flux.wavelength.unit.to_string()
 
 # Extract nocoverage map
 ha = maps.emline_gflux_ha_6564
-nocov = ha.masked
+
+for attr in ATTRS:
+	curr_ha = getattr(maps, attr)
+	ha = ha + curr_ha
+
+nocov = ha.masked.mask
 
 side_len = int(cube.header['NAXIS1'])
 
@@ -114,12 +144,14 @@ for n in range(N):
 
 	ax_m.set_xlabel(xunit)
 	ax_m.set_ylabel(yunit)
+	ax_m.grid(b=True)
 
 	ax_b.plot(xvals, resid[n], color='r', linewidth=0.5, label='Residual')
 	ax_b.legend()
 
 	ax_b.set_xlabel(xunit)
 	ax_b.set_ylabel(yunit)
+	ax_b.grid(b=True)
 
 	pp.savefig(fig)
 	#if not os.path.isdir(IFU):
